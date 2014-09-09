e333246a41f735b220079c053505f867159a7f19
Nicolai Czempin <contact@czempin.de>
2014-09-09 20:33:29 +0200

    initial (test/setup) import

---
+++ test1.asm
@@ -0,0 +1,7 @@
+*=$000
+	LDA $020	; load the value at address 0x20 into the accumulator
+	ADD $021	; add the value at address 0x21 to the accumulator
+	STA $100	; store the result (value of accumulator) at address $100 (RAM in the very first version of the machine)
+*=$020
+	.word $55AA
+	.word $1001
