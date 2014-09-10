ab91ad4f45a2b7c52bea865ad07e39edfb20268c
Nicolai Czempin <contact@czempin.de>
2014-09-10 21:22:30 +0200

    removed obsolete comments

--- test1.asm
+++ test1.asm
@@ -1,8 +1,8 @@
 *=$000
-	LDA $020	; load the value at address 0x20 into the accumulator
-	ADD $021	; add the value at address 0x21 to the accumulator
+	LDA $004	
+	ADD $005	
 	STA $100	; store the result (value of accumulator) at address $100 (RAM in the very first version of the machine)
 	DIO $815
-*=$020
+
 	.word $55AA
 	.word $1001
